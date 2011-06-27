b1c4466a4b5461c015a2a3fa65bd94aa8f72050d
Paul Battley <pbattley@gmail.com>
2011-06-27 15:31:39 +0100

    Now works nicely.

--- main.js
+++ main.js
@@ -1,58 +1,84 @@
 $(document).ready(function(){
-  var parameters = {
-    p: [],
-    t: 3
-  };
-
-  window.location.hash.slice(1).split(/&/).forEach(function(kv){
-    var kv_ = kv.split(/=/);
-    var k = kv_[0], v = kv_[1];
-    switch (typeof parameters[k]) {
-      case "object":
-        parameters[k].push(v);
-        break;
-      case "number":
-        parameters[k] = parseFloat(v);
-        break;
-      default:
-        parameters[k] = v;
-    }
-  });
-
-  console.log(JSON.stringify(parameters));
-
-  parameters.p.forEach(function(url){
-    $('body').append('<iframe src="' + url + '" />');
-  });
-
-  var iframes = $('iframe');
-  var selected = 0;
-
-  var move = function(el, x){
-    $(el).css('left', x + 'px')
+  var DEFAULTS = {
+    url: [],
+    framewait: 20,
+    showtime: 2000
   }
 
-  var show = function(el){
+  var parseHash = function(hash, template){
+    var result = template;
+    $(hash.slice(1).split('&')).each(function(i, kv){
+      var kv_ = kv.split('=');
+      var k = kv_[0], v = kv_[1];
+      switch (typeof result[k]) {
+        case "object":
+          result[k].push(v);
+          break;
+        case "number":
+          result[k] = parseFloat(v);
+          break;
+        default:
+          result[k] = v;
+      }
+    });
+    return result;
+  };
+
+  var createIframes = function(urls){
+    $(urls).each(function(ii, url){
+      $('body').append('<iframe src="' + url + '" />');
+    });
+    return $('iframe');
+  };
+
+  var rearrangeIframes = function(iframes, offset, width){
+    var current = offset % iframes.length;
+    iframes.each(function(i, el){
+      if (i == current) {
+        setX(el, 0);
+      } else {
+        setX(el, width);
+      }
+    });
+  };
+
+  var setX = function(el, x){
+    $(el).css('left', x + 'px');
+  };
+
+  var options  = parseHash(window.location.hash, DEFAULTS);
+  var iframes  = createIframes(options.url);
+  var selected = 0;
+  var offset   = 0;
+
+  console.log(JSON.stringify(options));
+
+  var showNext = function(){
     var t0 = new Date();
     var w = $(window).width();
+    var current = iframes[offset % iframes.length];
+    var next    = iframes[(offset + 1) % iframes.length];
+
     var slide = function(){
       var d = new Date() - t0;
-      var r = (d / w);
-      var x = w * Math.pow(Math.sin(r * Math.PI / 2), 2) - w;
-      console.log(d, r, x);
-      if (x >= -5) {
-        x = 0;
-      }
-      move(el, x);
-      if (x < 0) {
-        setTimeout(slide, 5);
+      var r = (0.6 * d / w);
+      var x = -w * Math.pow(Math.sin(r * Math.PI / 2), 2);
+      if (x > (5 - w)) { // fudge to lock to new position
+        setX(current, x);
+        setX(next, x + w);
+        setTimeout(slide, options.framewait);
+      } else {
+        offset = (offset + 1) % iframes.length;
+        rearrangeIframes(iframes, offset, w);
+        setTimeout(showNext, options.showtime);
       }
     };
     slide();
   };
 
+  rearrangeIframes(iframes, offset, $(window).width);
   setTimeout(function(){
-    show(iframes[0]);
-  }, 2000);
+    showNext();
+  }, options.showtime);
 
 });
